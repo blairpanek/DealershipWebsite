372d1c3eebdd897757c429fe69e5a0d611babb51
cody-elhard <32208000+cody-elhard@users.noreply.github.com>
2018-11-15 13:51:34 -0600

    Change it up

--- group_project/db_creation/startup.sql
+++ group_project/db_creation/startup.sql
@@ -44,7 +44,7 @@ Create Table DealershipUsers (
 
   Name VARCHAR(50) NOT NULL,
   Address VARCHAR(50) NOT NULL,
-  PhoneNumber VARCHAR(12) NOT NULL,
+  PhoneNumber VARCHAR(50) NOT NULL,
   Username VARCHAR(50) NOT NULL,
   Password VARCHAR(50) NOT NULL,
   Email VARCHAR(50) NOT NULL,
