d00a92ebbc137ae075162278fed2fbfb48a337b5
isaacflage <35605929+isaacflage@users.noreply.github.com>
2018-11-15 15:05:36 -0600

    Update startup.sql
    Wrong commit before, this should fix databases

--- group_project/db_creation/startup.sql
+++ group_project/db_creation/startup.sql
@@ -1,9 +1,9 @@
-DROP TABLE Users;
-DROP TABLE Vehicles;
-DROP TABLE Dealerships;
-DROP TABLE DealershipUsers;
-DROP TABLE DealershipVehicleConnection;
-DROP TABLE UserPayments;
+DROP TABLE Users CASCADE CONSTRAINTS;
+DROP TABLE Vehicles CASCADE CONSTRAINTS;
+DROP TABLE Dealerships CASCADE CONSTRAINTS;
+DROP TABLE DealershipUsers CASCADE CONSTRAINTS;
+DROP TABLE DealershipVehicleConnection CASCADE CONSTRAINTS;
+DROP TABLE UserPayments CASCADE CONSTRAINTS;
 
 Create Table Users (
   User_ID int NOT NULL,
@@ -12,8 +12,7 @@ Create Table Users (
   Password VARCHAR(50) NOT NULL,
   PhoneNumber int NOT NULL,
   Email VARCHAR(50) NOT NULL,
-  PRIMARY KEY (User_ID))
-  ;
+  PRIMARY KEY (User_ID));
 
 Create Table Vehicles (
   Vehicle_ID int NOT NULL,
@@ -21,15 +20,13 @@ Create Table Vehicles (
   Model VARCHAR(50) NOT NULL,
   Year VARCHAR(50) NOT NULL,
   Mileage VARCHAR(50) NOT NULL,
-  PRIMARY KEY (Vehicle_ID))
-  ;
+  PRIMARY KEY (Vehicle_ID));
 
 Create Table Dealerships (
   Dealership_ID int NOT NULL,
   Name VARCHAR(50) NOT NULL,
   Location VARCHAR(50) NOT NULL,
-  PRIMARY KEY (Dealership_ID))
-  ;
+  PRIMARY KEY (Dealership_ID));
 
 Create Table DealershipUsers (
   DealershipUsers_ID int NOT NULL,
@@ -41,49 +38,28 @@ Create Table DealershipUsers (
   Password VARCHAR(50) NOT NULL,
   Email VARCHAR(50) NOT NULL,
   PRIMARY KEY (DealershipUsers_ID),
-    FOREIGN KEY (Dealership_ID) REFERENCES Dealerships(Dealership_ID)
-)
-;
+    FOREIGN KEY (Dealership_ID) REFERENCES Dealerships(Dealership_ID));
 
 Create Table DealershipVehicleConnection (
   DealershipVehicleConnection_ID int NOT NULL,
   Dealership_ID int NOT NULL,
   Vehicle_ID int NOT NULL,
-
   Price VARCHAR(7) NOT NULL,
-
   PRIMARY KEY (DealershipVehicleConnection_ID),
-
-  --Dealership_FK
-    FOREIGN KEY (Dealership_ID)
-    REFERENCES Dealership_PK(Dealership_ID),
-
-  -- Vehicle_FK
-    FOREIGN KEY (Vehicle_ID)
-    REFERENCES Vehicles(Vehicle_ID)
-)
-;
+    FOREIGN KEY (Dealership_ID) REFERENCES Dealerships(Dealership_ID),
+    FOREIGN KEY (Vehicle_ID) REFERENCES Vehicles(Vehicle_ID));
 
 Create Table UserPayments (
   UserPayments_ID int NOT NULL,
   Vehicle_ID int NOT NULL,
   User_ID int NOT NULL,
   Dealership_ID int NOT NULL,
-
-  UserPayments_PK PRIMARY KEY (UserPayments_ID),
-
-  --Vehicle_FK
+  PRIMARY KEY (UserPayments_ID),
     FOREIGN KEY (Vehicle_ID)
     REFERENCES Vehicles(Vehicle_ID),
-
-    --USER_FK
     FOREIGN KEY (User_ID)
     REFERENCES Users(User_ID),
-
-  --Dealership_FK
     FOREIGN KEY (Dealership_ID) REFERENCES
-    Dealerships(Dealership_ID)
-)
-;
+    Dealerships(Dealership_ID));
 
 INSERT INTO Users VALUES (1, 'user_name', 'user', '123', 123, 'user@mail.com');
