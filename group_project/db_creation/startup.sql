5826b238e2dc104a834abb3b403c3d5ed0dc169a
isaacflage <35605929+isaacflage@users.noreply.github.com>
2018-11-15 15:02:04 -0600

    Update startup.sql
    Fixed database issues (hopefully)

--- group_project/db_creation/startup.sql
+++ group_project/db_creation/startup.sql
@@ -8,15 +8,12 @@ DROP TABLE UserPayments;
 Create Table Users (
   User_ID int NOT NULL,
   Name VARCHAR(50) NOT NULL,
-  UserName VARCHAR(50) NOT NULL,
+  UserName VARCHAR(50) NOT NULL UNIQUE,
   Password VARCHAR(50) NOT NULL,
   PhoneNumber int NOT NULL,
   Email VARCHAR(50) NOT NULL,
-
-  CONSTRAINT USER_PK PRIMARY KEY (User_ID),
-
-  CONSTRAINT UserName_Unique UNIQUE (Username)
-);
+  PRIMARY KEY (User_ID))
+  ;
 
 Create Table Vehicles (
   Vehicle_ID int NOT NULL,
@@ -24,37 +21,29 @@ Create Table Vehicles (
   Model VARCHAR(50) NOT NULL,
   Year VARCHAR(50) NOT NULL,
   Mileage VARCHAR(50) NOT NULL,
-
-  CONSTRAINT Vehicle_PK PRIMARY KEY (Vehicle_ID)
-);
+  PRIMARY KEY (Vehicle_ID))
+  ;
 
 Create Table Dealerships (
   Dealership_ID int NOT NULL,
-
   Name VARCHAR(50) NOT NULL,
   Location VARCHAR(50) NOT NULL,
-
-  CONSTRAINT Dealership_PK PRIMARY KEY (Dealership_ID)
-);
+  PRIMARY KEY (Dealership_ID))
+  ;
 
 Create Table DealershipUsers (
   DealershipUsers_ID int NOT NULL,
-
   Dealership_ID int NOT NULL,
-
   Name VARCHAR(50) NOT NULL,
   Address VARCHAR(50) NOT NULL,
   PhoneNumber VARCHAR(50) NOT NULL,
   Username VARCHAR(50) NOT NULL,
   Password VARCHAR(50) NOT NULL,
   Email VARCHAR(50) NOT NULL,
-
-  CONSTRAINT DealershipUsers_PK PRIMARY KEY (DealershipUsers_ID),
-
-  CONSTRAINT Dealership_FK
-    FOREIGN KEY (Dealership_ID)
-    REFERENCES Dealerships(Dealership_ID)
-);
+  PRIMARY KEY (DealershipUsers_ID),
+    FOREIGN KEY (Dealership_ID) REFERENCES Dealerships(Dealership_ID)
+)
+;
 
 Create Table DealershipVehicleConnection (
   DealershipVehicleConnection_ID int NOT NULL,
@@ -63,16 +52,17 @@ Create Table DealershipVehicleConnection (
 
   Price VARCHAR(7) NOT NULL,
 
-  CONSTRAINT DealershipVehicleConnection_PK PRIMARY KEY (DealershipVehicleConnection_ID),
+  PRIMARY KEY (DealershipVehicleConnection_ID),
 
-  CONSTRAINT Dealership_FK
+  --Dealership_FK
     FOREIGN KEY (Dealership_ID)
     REFERENCES Dealership_PK(Dealership_ID),
 
-  CONSTRAINT Vehicle_FK
+  -- Vehicle_FK
     FOREIGN KEY (Vehicle_ID)
     REFERENCES Vehicles(Vehicle_ID)
-);
+)
+;
 
 Create Table UserPayments (
   UserPayments_ID int NOT NULL,
@@ -80,19 +70,20 @@ Create Table UserPayments (
   User_ID int NOT NULL,
   Dealership_ID int NOT NULL,
 
-  CONSTRAINT UserPayments_PK PRIMARY KEY (UserPayments_ID),
+  UserPayments_PK PRIMARY KEY (UserPayments_ID),
 
-  CONSTRAINT Vehicle_FK
+  --Vehicle_FK
     FOREIGN KEY (Vehicle_ID)
     REFERENCES Vehicles(Vehicle_ID),
 
-  CONSTRAINT USER_FK
+    --USER_FK
     FOREIGN KEY (User_ID)
     REFERENCES Users(User_ID),
 
-  CONSTRAINT Dealership_FK
+  --Dealership_FK
     FOREIGN KEY (Dealership_ID) REFERENCES
     Dealerships(Dealership_ID)
-);
+)
+;
 
 INSERT INTO Users VALUES (1, 'user_name', 'user', '123', 123, 'user@mail.com');
