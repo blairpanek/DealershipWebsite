901b947461bdb1c72eebc6f055de26a9400205b1
Cody Elhard <cody.elhard@ndsu.edu>
2018-11-15 18:18:20 -0600

    Navbar

--- group_project/group_project/users/index.php
+++ group_project/group_project/users/index.php
@@ -8,10 +8,12 @@
   <div class="container">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <a class="navbar-brand" href="index.php"> Home </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
           <li class="nav-item active">
             <a class="nav-link" href="profile.php"> Profile </a>
           </li>
