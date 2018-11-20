3598404df43c622cdd347897d9b5d9416e155ecd
cody-elhard <32208000+cody-elhard@users.noreply.github.com>
2018-11-20 13:18:36 -0600

    Update vehicles.php

--- group_project/group_project/users/vehicles.php
+++ group_project/group_project/users/vehicles.php
@@ -4,18 +4,22 @@ $conn = oci_connect('coelhard', 'Jan211999', '(DESCRIPTION=(ADDRESS_LIST=(ADDRES
 
 $dealership_id = $_GET["dealership_id"];
 
-echo $dealership_id;
-
 $query = "select * FROM Vehicles WHERE Dealership_ID = " . $dealership_id;
 $stid = oci_parse($conn, $query);
 oci_execute($stid);
 
 while (($row = oci_fetch_array($stid, OCI_BOTH)) != false) {
     echo 'Color: '   . $row[2];
+    echo '<br />';
     echo 'Model: '   . $row[3];
+    echo '<br />';
     echo 'Year: '    . $row[4];
+    echo '<br />';
     echo 'Mileage: ' . $row[5];
+    echo '<br />';
     echo 'Price: '   . $row[6];
+    echo '<hr />';
+    echo "<button type=\"button\" class=\"btn btn-primary\"> Buy Now! </button>";
 }
 
 oci_free_statement($stid);
@@ -25,9 +29,15 @@ oci_close($conn);
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title> Vehicles - Dealer View </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   </head>
   <body>
 
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   </body>
 </html>
