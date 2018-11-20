84c3a470b7b52435e4d438d4a58242d534cbb5ed
cody-elhard <32208000+cody-elhard@users.noreply.github.com>
2018-11-20 12:37:52 -0600

    Update vehicles.php

--- group_project/group_project/users/vehicles.php
+++ group_project/group_project/users/vehicles.php
@@ -10,7 +10,7 @@ oci_execute($stid);
 echo $dealership_id;
 
 while (($row = oci_fetch_array($stid, OCI_BOTH)) != false) {
-    echo "<a href=\"vehicles/?dealership_id=" . $row[0] . "\">" . $row[1] . "</a>";
+    echo "<a href=\"vehicles.php?dealership_id=" . $row[0] . "\">" . $row[1] . "</a>";
     echo "<hr />";
 }
 
