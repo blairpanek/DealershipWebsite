07741faa173465574aa2d70a1f4b5dcffafe8d9d
cody-elhard <32208000+cody-elhard@users.noreply.github.com>
2018-11-20 11:53:25 -0600

    List dealership test

--- group_project/group_project/users/dealership.php
+++ group_project/group_project/users/dealership.php
@@ -1,3 +1,25 @@
+<?php
+// Remember to replace 'username' and 'password'!
+$conn = oci_connect('coelhard', 'Jan211999', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db2.ndsu.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
+
+$query = "select * from Dealerships";
+$stid = oci_parse($conn, $query);
+oci_execute($stid);
+
+echo "<table border='1'>\n";
+while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+    echo "<tr>\n";
+    foreach ($row as $item) {
+        echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+    }
+    echo "</tr>\n";
+}
+echo "</table>\n";
+
+oci_free_statement($stid);
+oci_close($conn);
+?>
+
 <!DOCTYPE html>
 <html>
 </html>
