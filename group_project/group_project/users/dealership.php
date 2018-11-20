ee52eeb07da3b5255d2b40738fbb89f4360a97cc
cody-elhard <32208000+cody-elhard@users.noreply.github.com>
2018-11-20 12:05:49 -0600

    Update dealership.php

--- group_project/group_project/users/dealership.php
+++ group_project/group_project/users/dealership.php
@@ -6,15 +6,14 @@ $query = "select * from Dealerships";
 $stid = oci_parse($conn, $query);
 oci_execute($stid);
 
-echo "<table border='1'>\n";
+
 while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
-    echo "<tr>\n";
-    foreach ($row as $item) {
-        echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+    foreach ($x = 0; $x < $row.count(); $x++)
+    {
+      echo "<a href=\"vehicles/?dealership_id=" . $row[0] . "\"></a>";
     }
-    echo "</tr>\n";
+    echo "<br />";
 }
-echo "</table>\n";
 
 oci_free_statement($stid);
 oci_close($conn);
