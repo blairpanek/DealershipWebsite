885a894656f41ae465b9875fc83f76207f5dbc79
cody-elhard <32208000+cody-elhard@users.noreply.github.com>
2018-11-20 12:54:06 -0600

    Update vehicles.php

--- group_project/group_project/users/vehicles.php
+++ group_project/group_project/users/vehicles.php
@@ -3,6 +3,9 @@
 $conn = oci_connect('coelhard', 'Jan211999', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db2.ndsu.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
 
 $dealership_id = $_GET["dealership_id"];
+
+echo $dealership_id;
+
 $query = "select * FROM Vehicles WHERE Dealership_ID = " . $dealership_id;
 $stid = oci_parse($conn, $query);
 oci_execute($stid);
