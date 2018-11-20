87ae938c3f4c51b9448df2ab9ea389addf45489f
cody-elhard <32208000+cody-elhard@users.noreply.github.com>
2018-11-20 12:25:42 -0600

    Update dealership.php

--- group_project/group_project/users/dealership.php
+++ group_project/group_project/users/dealership.php
@@ -2,14 +2,14 @@
 // Remember to replace 'username' and 'password'!
 $conn = oci_connect('coelhard', 'Jan211999', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db2.ndsu.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
 
-$query = "select * from Dealerships";
+$query = "select Dealership_ID, Name from Dealerships";
 $stid = oci_parse($conn, $query);
 oci_execute($stid);
 
 while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
     for ($x = 0; $x < count($row); $x++) {
       echo "<a href=\"vehicles/?dealership_id=" . $row['Dealership_ID'] . "\">" . $row['Name'] . "</a>";
-      echo $row[0];
+      echo $row['Name'];
     }
     echo "<br />";
 }
