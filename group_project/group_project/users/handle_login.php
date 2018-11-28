a1e34b6dd10ae77fd2535ba56ee1170f68eecce6
Blair Panek <blair.panek@ndsu.edu>
2018-11-28 13:02:00 -0600

    reverted changes again.

--- group_project/group_project/users/handle_login.php
+++ group_project/group_project/users/handle_login.php
@@ -2,8 +2,8 @@
 // Remember to replace 'username' and 'password'!
 $conn = oci_connect('coelhard', 'Jan211999', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db2.ndsu.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
 
-$username = $_POST["username"];
-$password = $_POST["password"];
+$username = $_GET["username"];
+$password = $_GET["password"];
 $query = "select COUNT(users.username) from users where username='$username' and password='$password'";
 $stid = oci_parse($conn, $query);
 oci_execute($stid);
