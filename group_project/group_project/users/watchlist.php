2ccd27ed48f125ce49a62327fd54ee0739d362d3
isaacflage <35605929+isaacflage@users.noreply.github.com>
2018-11-27 19:15:01 -0600

    Added watchlist page

---
+++ group_project/group_project/users/watchlist.php
@@ -0,0 +1,42 @@
+<?php
+// Remember to replace 'username' and 'password'!
+$conn = oci_connect('iflage', 'Feb161998', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db2.ndsu.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
+
+//we will need to get userID from the session to ensure this is the correct watchlist
+$watchlist_id = "1";
+$query = "select * FROM UserVehicleWatchlist WHERE UserVehicleWatchlist_ID = " . $watchlist_id;
+$stid = oci_parse($conn, $query);
+
+$vehicleID = 0;
+oci_execute($stid);
+
+//sets vehicle ID for other query
+while (($row = oci_fetch_array($stid, OCI_BOTH)) != false) {
+  $vehicleID = $row[1];
+}
+oci_free_statement($stid);
+
+
+$query2 = "select * FROM Vehicles WHERE Vehicle_ID = " . $vehicleID;
+$stid2 = oci_parse($conn, $query2);
+oci_execute($stid2);
+
+while (($row = oci_fetch_array($stid2, OCI_BOTH)) != false) {
+  echo 'Color: '   . $row[2];
+  echo '<br />';
+  echo 'Model: '   . $row[3];
+  echo '<br />';
+  echo 'Year: '    . $row[4];
+  echo '<br />';
+  echo 'Mileage: ' . $row[5];
+  echo '<br />';
+  echo 'Price: '   . $row[6];
+  echo '<hr />';
+}
+oci_free_statement($stid2);
+oci_close($conn);
+?>
+
+<!DOCTYPE html>
+<html>
+</html>
