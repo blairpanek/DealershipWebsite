841b7d98a90ce964072631578d2699eece007fef
cody-elhard <32208000+cody-elhard@users.noreply.github.com>
2018-11-29 12:13:48 -0600

    Update index.php

--- group_project/group_project/users/index.php
+++ group_project/group_project/users/index.php
@@ -13,6 +13,7 @@
         <span class="navbar-toggler-icon"></span>
       </button>
 
+
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
