f676979e9c1828d6d45121e127495336f6f88c77
Cody Elhard <cody.elhard@ndsu.edu>
2018-12-02 10:58:31 -0600

    Update profile.php

--- group_project/group_project/users/profile.php
+++ group_project/group_project/users/profile.php
@@ -55,17 +55,19 @@ oci_execute($stid);
         Account Details
       </h1>
 
-      <?php
-        while (($row = oci_fetch_array($stid, OCI_BOTH)) != false) {
-            echo 'Name: '   . $row[0];
-            echo '<br />';
-            echo 'UserName: '   . $row[1];
-            echo '<br />';
-            echo 'Phone Number: '    . $row[3];
-            echo '<br />';
-            echo 'Email: ' . $row[4];
-          }
-       ?>
+      <div class="ml-3">
+        <?php
+          while (($row = oci_fetch_array($stid, OCI_BOTH)) != false) {
+              echo 'Name: '   . $row[0];
+              echo '<br />';
+              echo 'UserName: '   . $row[1];
+              echo '<br />';
+              echo 'Phone Number: '    . $row[3];
+              echo '<br />';
+              echo 'Email: ' . $row[4];
+            }
+         ?>
+      </div>
  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
