3814c498d9a5d3d2052aa5e5e8444f7d190b6782
Cody Elhard <cody.elhard@ndsu.edu>
2018-12-02 10:47:39 -0600

    Create links for user

--- group_project/group_project/users/profile.php
+++ group_project/group_project/users/profile.php
@@ -36,18 +36,12 @@ oci_execute($stid);
             <li class="nav-item active">
               <a class="nav-link" href="watchlist.php"> Watchlist </a>
             </li>
-      <?php
-              if(!$_SESSION['username'] == ''){
-                echo "<li class=\"nav-item active\">";
-                echo  "<a class=\"nav-link\" href=\"logout.php\"> Logout </a>";
-                echo "</li>";
-              }
-              else{
-                echo "<li class=\"nav-item active\">";
+            <?php
+              if ($username == '') {
                 echo  "<a class=\"nav-link\" href=\"login.php\"> Login </a>";
-                echo "</li>";
+              } else {
+                echo  "<a class=\"nav-link\" href=\"logout.php\"> Logout </a>";
               }
-
             ?>
           </ul>
         </div>
@@ -65,7 +59,6 @@ oci_execute($stid);
         echo '<br />';
         echo 'Email: ' . $row[4];
       }
-
    ?>
  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
