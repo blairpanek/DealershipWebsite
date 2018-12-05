1336b41409294afbe4b828ccde1a8ea24e8ccefc
blair.panek@myprowler.org <blair.panek@myprowler.org>
2018-12-05 00:01:48 -0600

    trying something funky

--- group_project/group_project/users/vehicles.php
+++ group_project/group_project/users/vehicles.php
@@ -5,7 +5,7 @@ session_start();
 
   $dealership_id = $_GET["dealership_id"];
   $username = $_SESSION['username'];
-  $query = "SELECT Vehicles.*, UserVehicleWatchlist.Vehicle_ID, UserVehicleWatchlist.UserName FROM (Vehicles LEFT JOIN UserVehicleWatchlist ON UserVehicleWatchlist.UserName='$username' AND Vehicles.Vehicle_ID = UserVehicleWatchlist.Vehicle_ID) WHERE Vehicles.Dealership_ID = $dealership_id";
+  $query = "SELECT Vehicles.*, UserVehicleWatchlist.Vehicle_ID, UserVehicleWatchlist.UserName FROM ((Vehicles LEFT JOIN UserVehicleWatchlist ON UserVehicleWatchlist.UserName='$username') EXCEPT SELECT UserVehicleWatchlist.Vehicle_ID FROM UserVehicleWatchlist WHERE UserVehicleWatchlist.Vehicle_ID = Vehicles.Vehicle_ID ) WHERE Vehicles.Dealership_ID = $dealership_id";
   $stid = oci_parse($conn, $query);
   oci_execute($stid);
 ?>
