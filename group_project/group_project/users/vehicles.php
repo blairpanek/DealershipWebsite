ebc95ba7cb4e3b0ee7009993a83213ae1c8ac623
blair.panek@myprowler.org <blair.panek@myprowler.org>
2018-12-04 23:42:34 -0600

    trying an EXCEPT key term

--- group_project/group_project/users/vehicles.php
+++ group_project/group_project/users/vehicles.php
@@ -5,7 +5,7 @@ session_start();
 
   $dealership_id = $_GET["dealership_id"];
   $username = $_SESSION['username'];
-  $query = "SELECT Vehicles.*, UserVehicleWatchlist.Vehicle_ID, UserVehicleWatchlist.UserName FROM Vehicles INNER JOIN UserVehicleWatchlist ON UserVehicleWatchlist.UserName='$username' AND Vehicles.Vehicle_ID = UserVehicleWatchlist.Vehicle_ID";
+  $query = "SELECT Vehicles.* FROM Vehicles EXCEPT SELECT Vehicles.* FROM  UserVehicleWatchlist";
   $stid = oci_parse($conn, $query);
   oci_execute($stid);
 ?>
