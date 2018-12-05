df08866b1cf00bc6d3d746418294c8c63c04d34c
blair.panek@myprowler.org <blair.panek@myprowler.org>
2018-12-05 00:12:50 -0600

    small change again

--- group_project/group_project/users/vehicles.php
+++ group_project/group_project/users/vehicles.php
@@ -5,7 +5,7 @@ session_start();
 
   $dealership_id = $_GET["dealership_id"];
   $username = $_SESSION['username'];
-  $innerJoinQuery = "SELECT Vehicles.*, UserVehicleWatchlist.Vehicle_ID, UserVehicleWatchlist.UserName From Vehicles INNER JOIN UserVehicleWatchlist ON Vehicles.Vehicle_ID = UserVehicleWatchlist.Vehicle_ID AND UserVehicleWatchlist.UserName = $username";
+  $innerJoinQuery = "SELECT Vehicles.*, UserVehicleWatchlist.Vehicle_ID, UserVehicleWatchlist.UserName From (Vehicles INNER JOIN UserVehicleWatchlist ON Vehicles.Vehicle_ID = UserVehicleWatchlist.Vehicle_ID AND UserVehicleWatchlist.UserName = $username)";
   $query = "SELECT Vehicles.* FROM (Vehicles EXCEPT $innerJoinQuery) WHERE Vehicles.Dealership_ID = $dealership_id";
   $stid = oci_parse($conn, $query);
   oci_execute($stid);
